5963d10f56ba0d365c13d58cb293ff11255d7e23
Aleksi GAUTIER <algautie@e1r6p19.42.fr>
2019-09-19 15:13:45 +0200

    bug fixes

--- sources/conv_p.c
+++ sources/conv_p.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: algautie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/18 12:09:39 by algautie          #+#    #+#             */
-/*   Updated: 2019/09/19 14:19:44 by algautie         ###   ########.fr       */
+/*   Created: 2019/09/19 14:44:01 by algautie          #+#    #+#             */
+/*   Updated: 2019/09/19 15:11:15 by algautie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,27 +37,45 @@ static void					print_width(t_pf *pf, char *str)
 	}
 }
 
-void						conv_p(t_pf *pf)
+static void					print_precision(t_pf *pf, char *str)
 {
-	char	*str;
-	int		i;
+	int len;
+	int precision;
 
-	if (!(str = ft_ulltoa_base(get_arg(pf), 16)))
-		pf->error = 1;
-	if (pf->error)
-		return ;
-	i = -1;
-	while (str[++i])
-		str[i] = ft_tolower(str[i]);
-	pf->preflag_minus ? 0 : print_width(pf, str);
+	precision = pf->precision;
+	len = ft_strlen(str);
 	if (str)
 	{
 		write(1, "0x", 2);
 		pf->len += 2;
 	}
-	ft_putstr(str);
-	pf->len += ft_strlen(str);
-	pf->preflag_minus ? print_width(pf, str) : 0;
+	while (precision > len)
+	{
+		write(1, "0", 1);
+		pf->len++;
+		precision--;
+	}
+	if (pf->precision != 0)
+	{
+		ft_putstr(str);
+		pf->len += ft_strlen(str);
+	}
+}
+
+void						conv_p(t_pf *pf)
+{
+	char	*str;
+	int		i;
+
+	i = -1;
+	if (!(str = ft_ulltoa_base(get_arg(pf), 16)))
+		pf->error = 1;
+	if (pf->error)
+		return ;
+	while (str[++i])
+		str[i] = ft_tolower(str[i]);
+	pf->preflag_minus == 1 ? print_precision(pf, str) : print_width(pf, str);
+	pf->preflag_minus == 0 ? print_precision(pf, str) : print_width(pf, str);
 	if (str)
 		free(str);
 }
